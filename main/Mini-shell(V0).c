b32c10d7e96f09a60c2d312d1fee28512adb61e0
Ludovic <hiroludovic@gmail.com>
2026-06-08 00:13:37 +0000

    add lib.h

--- main/Mini-shell(V0).c
+++ main/Mini-shell(V0).c
@@ -1,9 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <errno.h>
-#include <string.h>
-#include <sys/wait.h>
+#include "lib.h"
 
 int main(void)
 {
---
+++ main/lib.h
@@ -0,0 +1,12 @@
+#if !defined(LIB)
+#define LIB
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <signal.h>
+#include <sys/wait.h>
+#include <string.h>
+
+#endif // LIB
